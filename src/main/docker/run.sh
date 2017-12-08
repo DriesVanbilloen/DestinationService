e33da11eb3026a04d40b0b6515d1839db8f6aafe
r0630049 <vanbilloen.3s@hotmail.com>
2017-12-08 12:17:39 +0100

    Configuring the dockerhub

---
+++ src/main/docker/run.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+echo "********************************************************"
+echo "Starting License Server"
+echo "********************************************************"
+java -jar /usr/local/DestinationService/@project.build.finalName@.jar
